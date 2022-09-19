e95139ec443100b635916ca62737e320a924c59a
FelipeGabriel7 <felipegabfd@gmail.com>
2022-09-19 10:32:16 -0300

    new sessions and corrections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 const itens = document.querySelector('.itens-navbar');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-
 function  menuMobile(){
   mobileMenu.addEventListener('click', () => {
     itens.classList.toggle('active')
